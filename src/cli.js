abdffcdeb5665a3465aebb2741232fc6f676b5e6
Nick_K17 <nickkuhnovec@gmail.com>
2021-09-06 21:45:17 +0200

    stage4.2

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from "readline-sync";
 
-export const questionName = () => {
+const questionName = () => {
   const name = readlineSync.question("May I have you name? ");
   console.log(`Hello, ${name}!`);
 };
+export default questionName;
