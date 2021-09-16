8e3a72d0a5fc156c3c3bea4f76cecdba67a63cef
Nick_K17 <nickkuhnovec@gmail.com>
2021-09-16 23:43:04 +0200

    fix

--- src/games/prime-game.js
+++ src/games/prime-game.js
@@ -4,13 +4,13 @@ import game from '../index.js';
 const primeCheck = (num) => {
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
     if (num % i === 0) {
-      return "no";
+      return 'no';
     }
   }
-  return "yes";
+  return 'yes';
 };
 
-const description = `"yes" if given number is prime. Otherwise answer "no".`;
+const description = '"yes" if given number is prime. Otherwise answer "no".';
 
 const getGameData = () => {
   const question = getRandomNumber(2, 100);
